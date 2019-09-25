81101d62b0027d4d8cbf1f928968f785d25b8753
enmanuelpinate <enmanuelpinate@gmail.com>
2019-09-25 22:13:23 +0200

    click and press

--- gestures.java
+++ gestures.java
@@ -1,8 +1,15 @@
 import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebElement;
 
 import io.appium.java_client.TouchAction;
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import static java.time.Duration.ofSeconds;
+import static io.appium.java_client.touch.offset.ElementOption.element;
 
 public class gestures extends base {
 
@@ -10,12 +17,18 @@ public class gestures extends base {
 		// TODO Auto-generated method stub
 		
 		AndroidDriver<AndroidElement> driver=cap();
-		
-		driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Accessibility\"]").click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Views\"]").click();
 		TouchAction t=new TouchAction(driver);
 		
+		WebElement expandList=driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Expandable Lists\"]");
+		t.tap(tapOptions().withElement(element(expandList))).perform();
 		
+		driver.findElementByXPath("//android.widget.TextView[@content-desc=\"1. Custom Adapter\"]").click();
+		WebElement pn=driver.findElementByXPath("//android.widget.TextView[@text='People Names']");
+		t.longPress(longPressOptions().withElement(element(pn)).withDuration(ofSeconds(2))).release().perform();
 		
+		System.out.println(driver.findElementById("android:id/title").isDisplayed());
 	}
 
 }
